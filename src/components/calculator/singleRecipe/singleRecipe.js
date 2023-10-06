6941a78a744d12d7c15127193ce2880a83ec445d
Admin <chemistry03@ukr.net>
2023-10-06 17:19:04 +0300

    Add processing recipies with may outputs

--- src/components/calculator/singleRecipe/singleRecipe.js
+++ src/components/calculator/singleRecipe/singleRecipe.js
@@ -3,11 +3,12 @@ import { PriceGeter } from "../../../API/accessors/prices"
 import { priceParce } from "../../../API/parcers/prices"
 
 const round10 = (num) => Math.round(num * 100) / 100
+const milisecondToHours = (time) => time / 1000 / 60 / 60
 
 export const SingleRecipe = (props) => {
-    let price = props.price;
+    let [outputPrices, setOutputPrices] = useState([]);
     let recipe = props.recipe;
-    let [materialPrices, setMaterialPrices] = useState([])
+    let [inputPrices, setInputPrices] = useState([])
 
     useEffect(() => {
         let temp = [];
@@ -15,19 +16,30 @@ export const SingleRecipe = (props) => {
             recipe.Inputs.map(input => PriceGeter.getInstance().GetPrices(input.CommodityTicker, props.marketTicker)
                 .then(x => Object.assign(priceParce(x), {"Amount": input.Amount}))
                 .then(x => temp.push(x))))
-        .then(() => setMaterialPrices(temp))
+        .then(() => setInputPrices(temp)).then(() => temp = [])
+        .then(() => Promise.all(recipe.Outputs.map(output => PriceGeter.getInstance().GetPrices(output.CommodityTicker, props.marketTicker)
+            .then(x => Object.assign(priceParce(x), {"Amount": output.Amount}))
+            .then(x => temp.push(x)))))
+        .then(() => setOutputPrices(temp)).then(() => temp = [])
         
         return () => (1)},
-        [recipe.Inputs, props.recipe, props.marketTicker])
+        [recipe, props.recipe, props.marketTicker])
 
-    let highProductionCost = round10(materialPrices.reduce((acum, cur) => acum + (cur.Amount * cur.Ask), 0))
-    let lowProductionCost = round10(materialPrices.reduce((acum, cur) => acum + (cur.Amount * cur.Bid), 0))
-    let highProfit = round10(price.Ask * recipe.Outputs[0].Amount - lowProductionCost, -2)
-    let lowProfit = round10(price.Bid * recipe.Outputs[0].Amount - highProductionCost, -2)
+    let highInputsCost = round10(inputPrices.reduce((acum, cur) => acum + (cur.Amount * cur.Ask), 0))
+    let lowInputsCost = round10(inputPrices.reduce((acum, cur) => acum + (cur.Amount * cur.Bid), 0))
+
+    let highOutputsCost = round10(outputPrices.reduce((acum, cur) => acum + (cur.Amount * cur.Ask), 0))
+    let lowOutputsnCost = round10(outputPrices.reduce((acum, cur) => acum + (cur.Amount * cur.Bid), 0))
+
+    let highProfit = highOutputsCost - lowInputsCost
+    let lowProfit = lowOutputsnCost - highInputsCost
 
     return <p className="SingleRecipe">{recipe.RecipeName} <br/> 
-        Low Production Cost: {lowProductionCost}; High Production Cost: {highProductionCost} <br/>
+        Low Production Cost: {lowInputsCost}; High Production Cost: {highInputsCost} <br/>
         High Profit: {highProfit}; Low Profit: {lowProfit} <br/>
-        High Marginality: {round10(highProfit / lowProductionCost, -2)}; 
-        Low Marginality: {round10(lowProfit / highProductionCost, -2)}</p>
+        High Marginality: {round10(highProfit / lowInputsCost)}; 
+        Low Marginality: {round10(lowProfit / highInputsCost)} <br/>
+        High daily profit: {round10(highProfit * 24 / milisecondToHours(recipe.DurationMs))}; 
+        Low daily profit: {round10(lowProfit * 24 / milisecondToHours(recipe.DurationMs))}
+        </p>
 }
